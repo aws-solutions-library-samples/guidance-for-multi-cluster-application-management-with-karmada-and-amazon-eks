77e1bb7695589a9ab8ccec775c13964d60aa463c
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2023-12-22 16:17:20 -0800

    Update test_script.sh
    Added scriptlet to install AWS CLI

--- test_script.sh
+++ test_script.sh
@@ -1,1 +1,6 @@
 #TODO: add commands that automate Karmada solution deployment below
+#install AWS CLI
+cd /tmp
+curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
+unzip awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
